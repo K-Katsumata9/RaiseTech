09014b399d098d88ddc121953fd38346e0232a99
K-Katsumata9 <dowbler@icloud.com>
2023-09-05 22:59:27 +0900

    test

--- ansible/dynamic-inventory-script.py
+++ ansible/dynamic-inventory-script.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import boto3
 import json
+import os
 
 # AWSの認証情報を設定
-aws_access_key = $AWS_ACCESS_KEY_ID
-aws_secret_key = $AAWS_SECRET_ACCESS_KEY
-aws_region = $AWS_DEFAULT_REGION
+aws_access_key = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+aws_region = os.getenv('AWS_DEFAULT_REGION')
 
 # EC2クライアントの作成
 ec2 = boto3.client('ec2', region_name=aws_region, aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
@@ -18,7 +19,7 @@ inventory = {'raisetech13': {'hosts': []}}
 
 for reservation in response['Reservations']:
     for instance in reservation['Instances']:
-        private_ip = instance['PublicIpAddress']
-        inventory['raisetech13']['hosts'].append(private_ip)
+        public_ip = instance['PublicIpAddress']
+        inventory['raisetech13']['hosts'].append(public_ip)
 
 print(json.dumps(inventory))
